08ef78896d4a032e5f9007dcf0da2d95d21f4038
hkret <s9i089wd4s.github@happy2.e4ward.co>
2024-01-11 19:30:05 +0100

    use context manager for servers

--- backend/src/money_pilot.py
+++ backend/src/money_pilot.py
@@ -1,8 +1,29 @@
 #!/usr/bin/env /usr/bin/python
 
-from server.ws_server import main
-
 import asyncio
 
+from core.status import app, STATUS_UNCONFIGURED
+from core.config import get_db_config
+from core.db import get_db
+from server.ws_server import get_websocket
+
+
+async def main():
+    app.status = STATUS_UNCONFIGURED
+    db_cfg = get_db_config()
+    print(f"{app.status=}, {db_cfg=}")
+
+    async with (
+        get_db(db_cfg) as db,
+        # get_websocket() as ws,
+    ):
+        print("App running")
+
+        # await asyncio.Future()
+
+
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Stopped by KeyboardInterrupt")
