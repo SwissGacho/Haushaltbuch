6f9f5716472cad72a569060ad72eefa77d02eb7f
hkret <s9i089wd4s.github@happy2.e4ward.co>
2024-08-09 18:20:57 +0200

    test util

--- backend/src/core/util.py
+++ backend/src/core/util.py
@@ -6,6 +6,16 @@ from core.base_objects import ConfigDict
 from persistance.bo_descriptors import BODict
 
 
+class _classproperty:
+    "Property on class level (only getter implemented)"
+
+    def __init__(self, fget: Callable) -> None:
+        self.fget = fget
+
+    def __get__(self, owner_self, owner_cls=None):
+        return self.fget(owner_cls)
+
+
 def get_config_item(cfg: dict, key: str):
     "Extract a value from a dict of dicts using 'key' as '/' separated path."
     if not cfg:
@@ -16,16 +26,6 @@ def get_config_item(cfg: dict, key: str):
             return None
         sub_cfg = sub_cfg.get(key_part, {})
     return sub_cfg
-
-
-class _classproperty:
-    "Property on class level (only getter implemented)"
-
-    def __init__(self, fget: Callable) -> None:
-        self.fget = fget
-
-    def __get__(self, owner_self, owner_cls=None):
-        return self.fget(owner_cls)
 
 
 def update_dicts_recursively(
