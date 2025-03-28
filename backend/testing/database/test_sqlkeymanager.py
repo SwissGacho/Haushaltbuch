5927d1e19ce1373e37aeade55541460b72b2c4a8
hkret <s9i089wd4s.github@happy2.e4ward.co>
2025-03-28 17:53:02 +0100

    revert chane to keymanager unittests

--- backend/testing/database/test_sqlkeymanager.py
+++ backend/testing/database/test_sqlkeymanager.py
@@ -8,34 +8,34 @@ from database.sqlkeymanager import SQLKeyManager
 class Test_100_SQLKeyManager(unittest.TestCase):
 
     def setUp(self):
-        self.SQLKeyManager = SQLKeyManager()
+        self.sql_key_manager = SQLKeyManager()
 
     def test_101_InitDefaults(self):
-        self.assertEqual(SQLKeyManager._last_key, 0)
-        self.assertEqual(self.SQLKeyManager._keys, set())
+        self.assertEqual(self.sql_key_manager._last_key, 0)
+        self.assertEqual(self.sql_key_manager._keys, set())
 
     def test_102_generate_key(self):
-        last_key = SQLKeyManager._last_key
-        key = self.SQLKeyManager._generate_key()
+        last_key = self.sql_key_manager._last_key
+        key = self.sql_key_manager._generate_key()
         self.assertEqual(key, "1")
-        self.assertEqual(SQLKeyManager._last_key, last_key + 1)
+        self.assertEqual(self.sql_key_manager._last_key, last_key + 1)
 
     def test_103_register_key(self):
-        key = self.SQLKeyManager.register_key("param")
+        key = self.sql_key_manager.register_key("param")
         self.assertEqual(key, "param")
-        self.assertEqual(self.SQLKeyManager._keys, {"param"})
+        self.assertEqual(self.sql_key_manager._keys, {"param"})
 
     def test_104_register_key_empty(self):
-        last_key = SQLKeyManager._last_key
-        key = self.SQLKeyManager.register_key("")
+        last_key = self.sql_key_manager._last_key
+        key = self.sql_key_manager.register_key("")
         self.assertEqual(key, str(last_key + 1))
-        self.assertEqual(self.SQLKeyManager._keys, {str(last_key + 1)})
+        self.assertEqual(self.sql_key_manager._keys, {str(last_key + 1)})
 
     def test_105_register_key_duplicate(self):
-        last_key = SQLKeyManager._last_key
-        self.assertEqual(self.SQLKeyManager.register_key("param"), "param")
-        key = self.SQLKeyManager.register_key("param")
+        last_key = self.sql_key_manager._last_key
+        self.assertEqual(self.sql_key_manager.register_key("param"), "param")
+        key = self.sql_key_manager.register_key("param")
         self.assertEqual(key, "param" + str(last_key + 1))
         self.assertEqual(
-            self.SQLKeyManager._keys, {"param", "param" + str(last_key + 1)}
+            self.sql_key_manager._keys, {"param", "param" + str(last_key + 1)}
         )
