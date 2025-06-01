1559daf7ba22ce20fca51fbae527bd265afbfbca
DrDiHai <sven.kindlimann@gmail.com>
2025-06-01 16:43:20 +0200

    Separated persistant and transient business objects

--- backend/src/data/management/configuration.py
+++ backend/src/data/management/configuration.py
@@ -6,13 +6,14 @@ from core.app_logging import getLogger, log_exit
 
 LOG = getLogger(__name__)
 
-from persistance.business_object_base import BOBase
+from persistance.persistant_business_object import PersistentBusinessObject
 from persistance.bo_descriptors import BODict, BORelation
 from data.management.user import User
 
 
-class Configuration(BOBase):
+class Configuration(PersistentBusinessObject):
     "Persistant configuration (global or user specific)"
+
     user_id = BORelation(User)
     configuration = BODict()
 
