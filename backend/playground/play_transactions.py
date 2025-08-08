99ed75256b94ce681da0e3471644e08ed6acfc3d
hkret <s9i089wd4s.github@happy2.e4ward.co>
2025-08-08 11:58:47 +0200

    added several features (see h)

--- backend/playground/play_transactions.py
+++ backend/playground/play_transactions.py
@@ -1,4 +1,5 @@
 import asyncio
+from encodings.punycode import T
 import re
 import sys
 import os
@@ -12,9 +13,7 @@ from core.configuration.db_config import DBConfig
 
 from database.dbms.sqlite import SQLiteDB
 from database.dbms.mysql import MySQLDB
-from database.sql import SQLConnection
-from database.sql_statement import SQLTemplate
-from database.sql_clause import Values
+from database.sql import SQL, SQLConnection, SQLTransaction
 
 SQLiITE_CONFIG = {
     "db": "SQLite",
@@ -34,6 +33,7 @@ CLEAN = "delete from :table"
 TABLES = ["toy_one", "toy_two", "toy_three"]
 
 print_traceback = False
+table = 1
 
 
 def get_single_key():
@@ -68,6 +68,7 @@ def help():
     print("  1: Use table 'toy_one'")
     print("  2: Use table 'toy_two'")
     print("  3: Use table 'toy_three'")
+    print("  k: Toggle 'keep connection' mode")
     print("  t: Toggle traceback printing")
     print("  b: Begin transaction")
     print("  c: Commit transaction")
@@ -79,18 +80,18 @@ def help():
 
 
 async def select(ctx, tab=1, all=False):
-    async with ctx.sql() as sql:
+    async with ctx.sql() if ctx else SQL() as sql:
         sel = re.sub(":table", TABLES[tab - 1], SELECTALL if all else SELECT)
         print(f"Executing: {sel}")
         print(await (await sql.script(sel).execute()).fetchall())
 
 
 async def insert(ctx, tab=1):
-    async with ctx.sql() as sql:
+    async with ctx.sql() if ctx else SQL() as sql:
         ins = re.sub(":table", TABLES[tab - 1], INSERT)
         print(f"Trying: {ins}")
         try:
-            print(await (await sql.script(ins).execute()).fetchall())
+            print(f"Returning: {await (await sql.script(ins).execute()).fetchall()}")
         except OperationalError as e:
             print(f"-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i-i> Error during insert: {e}")
             if print_traceback:
@@ -101,14 +102,141 @@ async def insert(ctx, tab=1):
 async def cleanup(ctx, tab=1):
     """Cleanup the database by deleting all entries in table."""
     clean = re.sub(":table", TABLES[tab - 1], CLEAN)
-    async with ctx.sql() as sql:
+    async with ctx.sql() if ctx else SQL() as sql:
         print(f"Executing: {clean}")
         await sql.script(clean).execute()
         print("Cleanup done.")
 
 
-async def main():
+async def transaction(conn):
+    global table
+    try:
+        async with conn.transaction() if conn else SQLTransaction() as trx:
+            print(
+                "_____________________________________________________________________________ Transaction started."
+                f"{'' if conn else ' (in own connection)'}"
+            )
+            while True:
+                key = await asyncio.get_event_loop().run_in_executor(
+                    None, get_single_key
+                )
+                if key in ["q", "\x03"]:
+                    print("Aborting transaction...")
+                    raise KeyboardInterrupt
+                elif key == "h":
+                    help()
+                elif key in ["1", "2", "3"]:
+                    table = int(key)
+                    print(f"Using table '{TABLES[table-1]}'")
+                elif key == "c":
+                    print("Exiting transaction...")
+                    break
+                elif key == "r":
+                    print("Rolling back transaction...")
+                    raise RollBackRequested
+                elif key in ["s", "*"]:
+                    await select(trx, table, all=(key == "*"))
+                elif key == "i":
+                    await insert(trx, table)
+                else:
+                    print(f"? ({key} unknown or not allowed in transaction)")
+                    continue
+    except KeyboardInterrupt:
+        print("Transaction aborted by user.")
+    except OperationalError as e:
+        print(
+            f"-t-t-t-t-t-t-t-t-t-t-t-t-t-t-t-t-t-t-t> '{e.__class__.__name__}' during transaction: {e}"
+        )
+        if print_traceback:
+            traceback.print_exc()
+    except RollBackRequested:
+        print("Rolled back transaction")
+    except Exception as e:
+        print(
+            f"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!> Unexpected '{e.__class__.__name__}' exception during transaction: {e}"
+        )
+        if print_traceback:
+            traceback.print_exc()
+    finally:
+        print(
+            "_____________________________________________________________________________ Exiting transaction context."
+        )
+
+
+async def mainloop(conn):
     global print_traceback
+    global table
+    try:
+        while True:
+            try:
+                key = await asyncio.get_event_loop().run_in_executor(
+                    None, get_single_key
+                )
+                # print(f"\nKey pressed: {key}")
+                if key in ["q", "\x03", "k"]:
+                    break
+                elif key == "h":
+                    help()
+                elif key in ["1", "2", "3"]:
+                    table = int(key)
+                    print(f"Using table '{TABLES[table-1]}'")
+                elif key == "t":
+                    print_traceback = not print_traceback
+                    print(
+                        f"Traceback printing is now {'enabled' if print_traceback else 'disabled'}."
+                    )
+                elif key == "b":
+                    await transaction(conn)
+                elif key == "c":
+                    try:
+                        await conn.commit()
+                        print("Transaction committed.")
+                    except OperationalError as e:
+                        print(
+                            f"--------------------------------------> '{e.__class__.__name__}' during commit: {e}"
+                        )
+                        if print_traceback:
+                            traceback.print_exc()
+                elif key == "r":
+                    try:
+                        await conn.rollback()
+                        print("Transaction rolled back.")
+                    except OperationalError as e:
+                        print(
+                            f"--------------------------------------> '{e.__class__.__name__}' during rollback: {e}"
+                        )
+                        if print_traceback:
+                            traceback.print_exc()
+                elif key in ["s", "*"]:
+                    await select(conn, table, all=(key == "*"))
+                elif key == "i":
+                    await insert(conn, table)
+                elif key == "x":
+                    await cleanup(conn, table)
+                    await conn.commit()
+                else:
+                    print(f"? ({key})")
+            except OperationalError as e:
+                print(
+                    f"--------------------------------------> '{e.__class__.__name__}' during execution: {e}"
+                )
+                if print_traceback:
+                    traceback.print_exc()
+            except Exception as e:
+                print(
+                    f"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!> Unexpected '{e.__class__.__name__}': {e}"
+                )
+                traceback.print_exc()
+    except Exception as e:
+        print(
+            f"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!> Unexpected and unhandled '{e.__class__.__name__}' outside loop: {e}"
+        )
+        traceback.print_exc()
+    finally:
+        return key
+
+
+async def main():
     print("Starting playing with DB...")
     while (db := input("DB (l/m): ").strip().lower()) not in ["l", "m"]:
         print("Please enter 'l' for SQLite or 'm' for MariaDB.")
@@ -118,130 +246,23 @@ async def main():
         App.db = MySQLDB(**MARIADB_CONFIG)
         DBConfig.db_configuration = {"db_cfg": MARIADB_CONFIG}
     print("   press 'h' for help or 'q' for quit")
-    tab = 1
-    async with SQLConnection() as conn:
-        try:
-            while True:
-                try:
-                    key = await asyncio.get_event_loop().run_in_executor(
-                        None, get_single_key
-                    )
-                    # print(f"\nKey pressed: {key}")
-                    if key in ["q", "\x03"]:
-                        break
-                    elif key == "h":
-                        help()
-                    elif key in ["1", "2", "3"]:
-                        tab = int(key)
-                        print(f"Using table '{TABLES[tab-1]}'")
-                    elif key == "t":
-                        print_traceback = not print_traceback
-                        print(
-                            f"Traceback printing is now {'enabled' if print_traceback else 'disabled'}."
-                        )
-                    elif key == "b":
-                        try:
-                            async with conn.transaction() as trx:
-                                print(
-                                    "_____________________________________________________________________________ Transaction started."
-                                )
-                                while True:
-                                    key = (
-                                        await asyncio.get_event_loop().run_in_executor(
-                                            None, get_single_key
-                                        )
-                                    )
-                                    if key in ["q", "\x03"]:
-                                        print("Exiting transaction.")
-                                        raise KeyboardInterrupt
-                                    elif key in ["b", "x"]:
-                                        print("Not allowed in transaction")
-                                        continue
-                                    elif key == "h":
-                                        help()
-                                    elif key in ["1", "2", "3"]:
-                                        tab = int(key)
-                                        print(f"Using table '{TABLES[tab-1]}'")
-                                    elif key == "c":
-                                        break
-                                    elif key == "r":
-                                        print("Rolling back transaction...")
-                                        raise RollBackRequested
-                                    elif key == "s":
-                                        await select(trx, tab)
-                                    elif key == "*":
-                                        await select(trx, tab, all=True)
-                                    elif key == "i":
-                                        await insert(trx, tab)
-                        except KeyboardInterrupt:
-                            print("Transaction aborted by user.")
-                        except OperationalError as e:
-                            print(
-                                f"-t-t-t-t-t-t-t-t-t-t-t-t-t-t-t-t-t-t-t> '{e.__class__.__name__}' during transaction: {e}"
-                            )
-                            if print_traceback:
-                                traceback.print_exc()
-                        except RollBackRequested:
-                            print("Rolled back transaction")
-                        except Exception as e:
-                            print(
-                                f"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!> Unexpected '{e.__class__.__name__}' exception during transaction: {e}"
-                            )
-                            if print_traceback:
-                                traceback.print_exc()
-                        finally:
-                            print(
-                                "_____________________________________________________________________________ Exiting transaction context."
-                            )
-                    elif key == "c":
-                        try:
-                            await conn.commit()
-                            print("Transaction committed.")
-                        except OperationalError as e:
-                            print(
-                                f"--------------------------------------> '{e.__class__.__name__}' during commit: {e}"
-                            )
-                            if print_traceback:
-                                traceback.print_exc()
-                    elif key == "r":
-                        try:
-                            await conn.rollback()
-                            print("Transaction rolled back.")
-                        except OperationalError as e:
-                            print(
-                                f"--------------------------------------> '{e.__class__.__name__}' during rollback: {e}"
-                            )
-                            if print_traceback:
-                                traceback.print_exc()
-                    elif key == "s":
-                        await select(conn, tab)
-                    elif key == "*":
-                        await select(conn, tab, all=True)
-                    elif key == "i":
-                        await insert(conn, tab)
-                    elif key == "x":
-                        await cleanup(conn, tab)
-                        await conn.commit()
-                    else:
-                        print(f"? ({key})")
-                except OperationalError as e:
-                    print(
-                        f"--------------------------------------> '{e.__class__.__name__}' during transaction: {e}"
-                    )
-                    if print_traceback:
-                        traceback.print_exc()
-                except Exception as e:
-                    print(
-                        f"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!> Unexpected '{e.__class__.__name__}': {e}"
-                    )
-                    traceback.print_exc()
-        except Exception as e:
+
+    keep_conn = True
+    key = ""
+    while key not in ["q", "\x03"]:
+        if keep_conn:
+            async with SQLConnection() as conn:
+                print("....................... Connection established.")
+                key = await mainloop(conn)
+            print("....................... Connection closed.")
+        else:
+            key = await mainloop(None)
+        if key == "k":
+            keep_conn = not keep_conn
             print(
-                f"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!> Unexpected and unhandled '{e.__class__.__name__}' outside loop: {e}"
+                f"Keep connection mode is now {'enabled' if keep_conn else 'disabled'}."
             )
-            traceback.print_exc()
-        finally:
-            print("Bye")
+    print("Bye")
 
 
 if __name__ == "__main__":
