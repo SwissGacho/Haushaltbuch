7eb1895ad79aa22090c96adc1c11356780085f91
hkret <s9i089wd4s.github@happy2.e4ward.co>
2026-01-22 17:37:01 +0100

    test build

--- backend/src/core/const.py
+++ backend/src/core/const.py
@@ -1,6 +1,5 @@
 """Global constants"""
 
-VERSION = "0.1.0"
 APPNAME = "moneypilot"
 APPDESC = f"""'{APPNAME}' helps you keeping control over the money you have available.
             Record your expenses and compare to your planned budget.
