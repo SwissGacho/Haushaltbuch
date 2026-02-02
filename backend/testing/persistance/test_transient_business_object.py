f9077fce5b502bcaba07a9869abaee51f0579d8e
DrDiHai <sven.kindlimann@gmail.com>
2026-02-03 00:07:46 +0100

    Final tests for transient_business_objects

--- backend/testing/persistance/test_transient_business_object.py
+++ backend/testing/persistance/test_transient_business_object.py
@@ -28,22 +28,28 @@ class Test_100_Transient_Business_Object_classmethods(unittest.IsolatedAsyncioTe
 
     async def test_101_instance_creation(self):
         """Test that instances are created and added to the weakref set."""
+        TransientBusinessObject._instances.clear()
         obj1 = TransientBusinessObject(bo_id=1)
         obj2 = TransientBusinessObject(bo_id=2)
 
-        assert len(TransientBusinessObject._instances) == 2
-        assert obj1 in TransientBusinessObject._instances
-        assert obj2 in TransientBusinessObject._instances
+        self.assertIsNotNone(obj1.id)
+        self.assertNotEqual(obj2.id, obj1.id)
+
+        self.assertEqual(len(TransientBusinessObject._instances), 2)
+        self.assertIn(obj1, TransientBusinessObject._instances)
+        self.assertIn(obj2, TransientBusinessObject._instances)
 
         obj1 = None
         gc.collect()
+        print(TransientBusinessObject._instances)
         self.assertEqual(len(TransientBusinessObject._instances), 1)
 
     async def test_102_count_rows(self):
         """Test the count_rows method."""
-        # TransientBusinessObject._instances.clear()
-        obj1 = TransientBusinessObject(bo_id=1)
-        obj2 = TransientBusinessObject(bo_id=2)
+        TransientBusinessObject._instances.clear()
+        # Must keep reference, else it gets garbage collected
+        bo1 = TransientBusinessObject(bo_id=1)
+        bo2 = TransientBusinessObject(bo_id=2)
 
         count = await TransientBusinessObject.count_rows()
 
