f51c2c3a8a974ec78e6c95f5d451413bf60822da
hkret <s9i089wd4s.github@happy2.e4ward.co>
2026-08-16 18:26:26 +0200

    remove duplicated code

--- backend/src/database/sql_statement.py
+++ backend/src/database/sql_statement.py
@@ -201,25 +201,6 @@ class SQLSubquery(SQLExpression):
     def get_query(self, km: SQLKeyManager) -> str:
         """Get the SQL query for this expression."""
         if isinstance(self._subquery, str):
-            return self._subquery
-        query_params = self._subquery.get_sql()
-        return f"({ km.merge_params(**query_params)})"
-
-
-class SQLSubquery(SQLExpression):
-    """Represents a SQL subquery."""
-
-    def __init__(self, subquery: str | Select):
-        if isinstance(subquery, str) or isinstance(subquery, Select):
-            self._subquery = subquery
-        else:
-            raise TypeError(
-                f"SQLSubquery.__init__: Expected str or Select, got {type(subquery)}"
-            )
-
-    def get_query(self, km: SQLKeyManager) -> str:
-        """Get the SQL query for this expression."""
-        if isinstance(self._subquery, str):
             return f"({self._subquery})"
         query_params = self._subquery.get_sql()
         return f"({km.merge_params(**query_params)})"
